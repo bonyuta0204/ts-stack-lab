2b727ce29a9b09d528dc29673626e469b072d906
Yuta Nakamura <nakamurayuta0204@gmail.com>
2024-11-30 17:22:55 +0900

    fix: eslint and type-check

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,21 +1,27 @@
-import { Router } from 'express';
-import { getUsers, getUser, createUser, updateUser, deleteUser } from '../controllers/userController';
+import { Router } from "express";
+import {
+  getUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} from "../controllers/userController";
 
 const router = Router();
 
 // Get all users
-router.get('/', getUsers);
+router.get("/", getUsers);
 
 // Get single user
-router.get('/:id', getUser);
+router.get("/:id", getUser);
 
 // Create user
-router.post('/', createUser);
+router.post("/", createUser);
 
 // Update user
-router.put('/:id', updateUser);
+router.put("/:id", updateUser);
 
 // Delete user
-router.delete('/:id', deleteUser);
+router.delete("/:id", deleteUser);
 
 export default router;
