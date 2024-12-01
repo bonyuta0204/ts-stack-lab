c630e53fa1f5395ed41fb897a8c91aa690f4f0c9
Yuta Nakamura <nakamurayuta0204@gmail.com>
2024-12-02 01:18:48 +0900

    Add firebase

--- server/src/config/firebase.ts
+++ server/src/config/firebase.ts
@@ -1,16 +1,16 @@
-import { initializeApp, cert, type App } from 'firebase-admin/app';
-import { getAuth } from 'firebase-admin/auth';
+import {
+  initializeApp,
+  type App,
+  applicationDefault,
+} from "firebase-admin/app";
+import { getAuth } from "firebase-admin/auth";
 
 let firebaseApp: App;
 
 export const initializeFirebase = () => {
   if (!firebaseApp) {
     firebaseApp = initializeApp({
-      credential: cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-      }),
+      credential: applicationDefault(),
     });
   }
   return firebaseApp;
